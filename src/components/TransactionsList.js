b2cf72d8c44ae3d6c583a65845e689c324b4208f
Noela-Murugi <murugin254@gmail.com>
2022-09-25 12:31:28 +0300

    updated

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,28 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">Date</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Description</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Category</h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">Amount</h3>
-          </th>
-        </tr>
-        {/* render a list of <Transaction> components here */}
-      </tbody>
-    </table>
-  );
+function TransactionsList({transactions,deleteTransactions, setTransactions, search}) {
+
+
+ 
 }
 
 export default TransactionsList;
