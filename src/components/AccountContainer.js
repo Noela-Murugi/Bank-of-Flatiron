abd5403d95bd988c1f865b7e7e5ab05d74171ac4
Noela-Murugi <murugin254@gmail.com>
2022-09-25 16:52:45 +0300

    update account contaioner

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -1,39 +1,37 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-
-  const [transactions, setTransactions] = useState([])
-  const [search, setSearch] = useState("")
-  // fetch transaction
-  useEffect(() =>{
-    fetch ("http://localhost:8001/transactions")
-    .then((response)=>response.json())
-    .then(data => setTransactions(data));
-
-  },[])
+  const [transactions, setTransactions] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data));
+  }, []);
   console.log(transactions);
-//new transaction
-function infor (newTransaction) {
-  const updatedTransactions = [...transactions, newTransaction]
-  setTransactions(updatedTransactions);
-}
 
+  function infor(newTransaction) {
+    const update = [...transactions, newTransaction];
+    setTransactions(update);
+  }
 
-//Delete transaction
-function deleteTransact(id) {
-  const updatedTransactions = transactions.filter(transaction => transaction.id !== id);
-  setTransactions(updatedTransactions);
-}
-
+  //Delete transaction
+  function deleteTransact(id) {
+    const update = transactions.filter((transaction) => transaction.id !== id);
+    setTransactions(update);
+  }
 
   return (
     <div>
-      <Search search={search} setSearch={setSearch}/>
-      <AddTransactionForm newTransactions={infor}/>
-      <TransactionsList transactions={transactions} deleteTransactions={deleteTransact}/>
+      <Search search={search} setSearch={setSearch} />
+      <AddTransactionForm transactions={transactions} newTransactions={infor} />
+      <TransactionsList
+        transactions={transactions}
+        deleteTransactions={deleteTransact}
+      />
     </div>
   );
 }
