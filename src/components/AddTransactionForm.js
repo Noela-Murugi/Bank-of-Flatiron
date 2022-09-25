ce000c2ef4fb0c7704929b8e72b0f3e1e1cf4561
Noela-Murugi <murugin254@gmail.com>
2022-09-25 12:48:49 +0300

    updated

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,9 +1,16 @@
 import React , {useState} from "react";
 
 
-function AddTransactionForm(newTransactions) {
+function AddTransactionForm(newTransactions,id) {
 
- 
+  const [dataInfo, setDataInfo] = useState({
+    date:"",
+    description:"",
+    category:"",
+    amount:0,
+  })
+
+
 }
 
 export default AddTransactionForm;
